1c8e6c90acaeaf62176bb17add4d74ccb94d3e19
Menachem Geva <me.geva@gmail.com>
2024-01-04 18:17:24 +0200

    Update read_data.py

--- dynamodb/read_data.py
+++ dynamodb/read_data.py
@@ -6,5 +6,7 @@ df = wr.dynamodb.read_items(
 
 #because createdBy is a dictionary 
 df['contains_Tal'] = df['createdBy'].apply(lambda x: 'Tal' in x.get('givenName', ''))
+df['utc_time'] = df['createdAt'].apply(lambda x: datetime.datetime.utcfromtimestamp(int(x)/1000))
 
-df[df.contains_Tal]
+
+df[df.contains_Tal].sort_values(by="utc_time")
